6b717c4f3e4a880584f6013603ccacf61b47ee25
leonxi <leon_xi@163.com>
2018-03-26 07:25:40 +0000

    routes

--- routes/project.js
+++ routes/project.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-rouer.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.json({ message: 'hooray! welcome to our api!' });
 });
 
 module.exports = router;
