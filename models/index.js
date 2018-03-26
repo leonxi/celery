2be957ccdccc789d85f05efe8e2df8dfc2ac25ba
leonxi <leon_xi@163.com>
2018-03-26 09:31:28 +0000

    mongoose

---
+++ models/index.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
+db.on('error', function callback() {
+  console.log("Connection error");
+});
+
+var host = process.env.MONGO_HOST || 'mongodb';
+var port = process.env.MONGO_PORT || '27017';
+var database = process.env.MONGO_DATABASE || 'celery';
+
+var username = process.env.MONGO_USERNAME || 'root';
+var password = process.env.MONGO_PASSWORD || '1234';
+
+mongoose.connect('mongodb://' + username + ':' + password + '@' + host + ':' + port + '/' + database);
+
+
+require('./project');
+
+exports.Project         = mongoose.model('Project');
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+require('./models');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
