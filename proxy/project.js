8b434e0ffd244441fbe408560a8ed1dd0fdd2c4b
Leon <leon_xi@163.com>
2018-03-26 19:23:21 +0800

    Update project.js

--- proxy/project.js
+++ proxy/project.js
@@ -1,6 +1,9 @@
 var models = require('../models');
 var Project = models.Project;
 
+exports.getProjectsByUserId = function (userid, callback) {
+};
+
 exports.newAndSave = function (name, callback) {
   var project = new Project();
   project.name = name;
