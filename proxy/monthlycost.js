23157d2860b9a5eae91893ac0b051b2096a8beb4
Leon <leon_xi@163.com>
2018-03-27 22:48:26 +0800

    Debug

--- proxy/monthlycost.js
+++ proxy/monthlycost.js
@@ -11,7 +11,7 @@ exports.newAndSave = function (id, year, month, username, name, role, callback) 
   Project.findOne({_id: id}, ep.done(function (project) {
     if (project) {
       
-      MonthlyCost.findOne({project:{_id: project.id}, year: year, month: month}, ep.done(function(monthlycost) {
+      MonthlyCost.findOne({project:{_id: project._id}, year: year, month: month}, ep.done(function(monthlycost) {
         if (monthlycost) {
           function customizer(objValue, srcValue) {
             if (_.isArray(objValue)) {
